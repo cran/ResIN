194b371c267dc44b841a8e98b59831bbb89d6054
Philip Warncke <pwarncke@live.unc.edu>
2023-06-30 09:10:02 +0000

    version 1.1.0

--- R/lik_data.R
+++ R/lik_data.R
@@ -1,6 +1,6 @@
-#' Likert-type simulated toy data for ResIN package examples
+#' Likert-type simulated data for "ResIN" package examples
 #'
-#' An artificially created data-set (n=1000) of 12, 5-point Likert type ordinal data. Modeled on the basis of a standard normal data-generating process. Likert scales reflect 20% uncorrelated, homoskedastic measurement error.
+#' An artificially created data-set (n=1000) of 12, 5-point Likert data. Modeled on the basis of a standard normal data-generating process. Likert scales contain 20 percent uncorrelated, homoscedastic measurement error. This data-set is used for the examples in the "ResIN" package vignette.
 #'
 #' @docType data
 #'
@@ -15,6 +15,9 @@
 #' data(lik_data)
 #' head(lik_data)
 #'
+#'
 "lik_data"
+
+
 
 
--- inst/doc/ResIN-VIGNETTE.R
+++ inst/doc/ResIN-VIGNETTE.R
@@ -12,58 +12,67 @@ library(ResIN)
 
 
 ## ----simulate data, message=FALSE, warning=FALSE------------------------------
- set.seed(42)
- n <- 1000
- k <- 8
 
- latent_dgp <- rnorm(1000, 0, 1)
-
- ## Data for k manifest indicators measured with some error
- cont_data <- matrix(rep(latent_dgp, k), n, k)
- cont_data <- apply(cont_data, 2, function(x) {
-   x + rnorm(1000, 0, 0.2)}) ## 20% error
-
- ## Ordinal, 5-point Likert-scale items
- lik_maker <- function(x) {
-   sort(runif(x, min = -2, max = 2))}
-
- lik_data <- matrix(NA, n, k)
-
- for(i in 1:ncol(lik_data)) {
-   lik_data[, i] <- findInterval(cont_data[, i], vec = c(-Inf, lik_maker(4), Inf))
- }
-
+data(lik_data)
 
 
 ## ----run ResIN, message=FALSE, warning=FALSE----------------------------------
- # Apply the ResIN function to toy Likert data:
+# Apply the ResIN function to toy Likert data:
 
- output <- ResIN(lik_data, cor_method = "spearman", network_stats = TRUE)
+ResIN_output <- ResIN(lik_data, cor_method = "spearman", network_stats = TRUE)
 
 
-## ----plotting, message=FALSE, warning=FALSE, fig.align='center', fig.width=12, fig.height=9----
+## ----plotting nodes, message=FALSE, warning=FALSE, fig.align='center', fig.width=12, fig.height=9----
 
-# Create a basic outcome plot with ggplot
- output$ggplot_frame <- output$ggplot_frame[order(output$ggplot_frame$Strength,
-                                                  decreasing = FALSE), ]
- ResIN_plot <- ggplot2::ggplot(output$ggplot_frame) +
-   geom_curve(data = output$ggplot_frame, aes(x = from.x, xend = to.x, y = from.y,
-                                              yend = to.y, linewidth = weight,
-                                              color = Strength), curvature = 0.2) +
-   geom_point(aes(x = from.x, y = from.y, shape = as.factor(cluster)), size = 8)+
-   geom_point(aes(x = to.x, y = to.y), size = 8)+
-   geom_text(data = output$ggplot_frame, aes(x = from.x, y = from.y, label = from),
-             size = 3, color = "white") +
-   geom_text(data = output$ggplot_frame, aes(x = to.x, y = to.y, label = to),
-             size = 3, color = "white") +
-   ggtitle("ResIN example  plot")+
-   theme_dark()+
-   theme(axis.text.x = element_blank(), axis.title.x = element_blank(),
-         axis.text.y = element_blank(), axis.title.y = element_blank(),
-         axis.ticks = element_blank(), panel.grid.major = element_blank(),
-         panel.grid.minor = element_blank(), legend.position = "none",
-         legend.text = element_blank(), plot.title = element_text(hjust = 0.5))
+ggplot(ResIN_output$node_frame, aes(x = x, y = y))+
+  geom_point(size = 0.25, color = "grey")+
+  geom_smooth(method = "lm", se = FALSE, linetype = 2, color = "red")+
+  geom_label(data = ResIN_output$node_frame, aes(x = x, y = y, label = node_names, color = Strength), size = 5)+
+  ylim(c(-4, 4))+
+  theme_bw()+
+  ggtitle("Item-Response Node Latent Positions")
 
- ResIN_plot
  
+
+## ----plotting edges, message=FALSE, warning=FALSE, fig.align='center', fig.width=12, fig.height=9----
+
+edge_plot <- ggplot() +
+    geom_curve(data = ResIN_output$edgelist_frame, aes(x = from.x, xend = to.x, y = from.y,
+                yend = to.y, size = weight^5), curvature = 0.15, color = "darkgrey", alpha = 0.8) +
+    geom_label(data = ResIN_output$edgelist_frame, aes(x = from.x, y = from.y, label = from, color = Strength), size = 5) +
+    geom_label(data = ResIN_output$edgelist_frame, aes(x = to.x, y = to.y, label = to, color = Strength), size = 5) +
+                  ## Make sure to plot node labels both based on the to and from columns in the plotting frame
+    ggtitle("ResIN Network in 2D Latent Space")+
+    labs(color = "Node strength centrality: ")+
+    ylim(c(-4, 4))+
+    scale_size_continuous(guide = "none")+
+    theme_bw()+
+    theme(axis.text.x = element_blank(), axis.title.x = element_blank(),
+          axis.text.y = element_blank(), axis.title.y = element_blank(),
+          axis.ticks = element_blank(), panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(), legend.position = "bottom",
+          legend.text = element_blank(), plot.title = element_text(hjust = 0.5, size=18))
+
+
+edge_plot
+
+
+## ---- warning=FALSE, message=FALSE, mysize=TRUE, size='\\scriptsize'----------
+
+## Integration with qgraph package
+resin_qgraph <- ResIN_qgraph(lik_data, plot_graph = TRUE,
+                    qgraph_arglist = list(layout = "spring", maximum = 1, vsize = 4,
+                    DoNotPlot = TRUE, sampleSize = nrow(lik_data),
+                    title = "ResIN plot made with qgraph package",
+                    mar = c(1,1,1,1), normalize = FALSE))
+
+
+## ---- warning=FALSE, message=FALSE, mysize=TRUE, size='\\scriptsize'----------
+
+## Integration with igraph package
+resin_igraph <- ResIN_igraph(lik_data)
+
+set.seed(12)
+igraph::plot.igraph(resin_igraph$igraph_obj)
+
 
